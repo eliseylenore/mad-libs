1868f9c95d239b16a674b999919524d45fbd6fbd
Elise St Hilaire and Kaz Matthews <elise@>
2017-01-11 09:47:41 -0800

    Add code in order to place the input as text in the madlibs story

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,17 @@ $(document).ready(function(){
     var noun3Input = $("input#noun3").val();
     var verb-ing2Input = $("input#verb-ing2").val();
     var noun4Input = $("input#noun4").val();
-    
+
+    $(".town").text(townInput);
+    $(".person1").text(person1Input);
+    $(".adjective1").text(adjective2Input);
+    $(".noun1").text(noun1Input);
+    $(".noun2").text(noun2Input);
+    $(".person2").text(person2Input);
+    $(".verb-ing1").(verb-ing1Input);
+    $(".noun3").(noun3Input);
+    $(".verb-ing2").(verb-ing2Input);
+    $(".noun4").(noun4Input);
 
   })
 })
