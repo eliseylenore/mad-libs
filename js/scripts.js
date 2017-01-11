1ef6503066713c20d00d4fc3740572561b0c1ee5
Elise St Hilaire and Kaz Matthews <kaz@>
2017-01-11 09:43:22 -0800

    Add variables equal to the value of inputs

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("#blanks form").submit(function(){
+    var townInput = $("input#town").val();
+    var person1Input = $("input#person1").val();
+    var adjective1Input = $("input#adjective1").val();
+    var noun1Input = $("input#noun1").val();
+    var noun2Input = $("input#noun2").val();
+    var person2Input = $("input#person2").val();
+    var verb-ing1Input = $("input#verb-ing2").val();
+    var noun3Input = $("input#noun3").val();
+    var verb-ing2Input = $("input#verb-ing2").val();
+    var noun4Input = $("input#noun4").val();
+    
+
+  })
+})
