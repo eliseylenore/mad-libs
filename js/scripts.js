28637a31497604808202ad779b2d0b97349efc4b
Elise St Hilaire and Kaz Matthews <kaz@>
2017-01-11 10:13:15 -0800

    fix definition of variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,30 @@
-$(document).ready(function(){
-  $("#blanks form").submit(function(){
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+
     var townInput = $("input#town").val();
     var person1Input = $("input#person1").val();
     var adjective1Input = $("input#adjective1").val();
     var noun1Input = $("input#noun1").val();
     var noun2Input = $("input#noun2").val();
     var person2Input = $("input#person2").val();
-    var verb-ing1Input = $("input#verb-ing2").val();
+    var verbing1Input = $("input#verb-ing2").val();
     var noun3Input = $("input#noun3").val();
-    var verb-ing2Input = $("input#verb-ing2").val();
+    var verbing2Input = $("input#verb-ing2").val();
     var noun4Input = $("input#noun4").val();
 
     $(".town").text(townInput);
     $(".person1").text(person1Input);
-    $(".adjective1").text(adjective2Input);
+    $(".adjective1").text(adjective1Input);
     $(".noun1").text(noun1Input);
     $(".noun2").text(noun2Input);
     $(".person2").text(person2Input);
-    $(".verb-ing1").(verb-ing1Input);
-    $(".noun3").(noun3Input);
-    $(".verb-ing2").(verb-ing2Input);
-    $(".noun4").(noun4Input);
+    $(".verb-ing1").text(verbing1Input);
+    $(".noun3").text(noun3Input);
+    $(".verb-ing2").text(verbing2Input);
+    $(".noun4").text(noun4Input);
 
-  })
-})
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
